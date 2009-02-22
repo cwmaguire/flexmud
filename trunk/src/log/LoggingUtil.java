b06eb6ddfd0cb1ac8fe9d6034c9332c4953a7afe
cwmaguire <cwmaguire@2b8fd586-012b-11de-85c2-bb0660c75f22>
2009-02-22 23:51:09 +0000

    - logging and logging test - Preferences stub (sort of) - junit and log4j libs - project files

---
+++ trunk/src/log/LoggingUtil.java
@@ -0,0 +1,64 @@
+/*
+Copyright 2009 Chris Maguire (cwmaguire@gmail.com)
+
+MUD Cartographer is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MUD Cartographer is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MUD Cartographer.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package log;
+
+import config.pref.Preferences;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.io.File;
+import java.util.Properties;
+
+public class LoggingUtil {
+    public static void configureLogging(String loggingConfigurationFile) {
+        configureLogging(new File(loggingConfigurationFile));
+    }
+
+    public static void configureLogging(File loggingConfigurationFile) {
+        if (loggingConfigurationFile.canRead()) {
+            PropertyConfigurator.configure(loggingConfigurationFile.toString());
+        } else {
+            configureLogging();
+        }
+    }
+
+    public static void configureLogging() {
+        File log4jConfigurationFile = new File(Preferences.getPreference("log4j config file"));
+        if (log4jConfigurationFile.canRead()) {
+            PropertyConfigurator.configure(Preferences.getPreference("log4j config file"));
+        } else {
+            PropertyConfigurator.configure(createDefaultLoggingProperties());
+        }
+    }
+
+    private static Properties createDefaultLoggingProperties() {
+        Properties log4jProperties = new Properties();
+        log4jProperties.put("log4j.rootLogger", "DEBUG, rollingFileAppender");
+        log4jProperties.put("log4j.appender.rollingFileAppender", "org.apache.log4j.RollingFileAppender");
+        log4jProperties.put("log4j.appender.rollingFileAppender.File", "testLog4j.log");
+        log4jProperties.put("log4j.appender.rollingFileAppender.MaxFileSize", "100KB");
+        log4jProperties.put("log4j.appender.rollingFileAppender.MaxBackupIndex", "1");
+        log4jProperties.put("log4j.appender.rollingFileAppender.layout", "org.apache.log4j.PatternLayout");
+        log4jProperties.put("log4j.appender.rollingFileAppender.layout.ConversionPattern", "%-4r [%t] %-5p %c %x - %m%n");
+        return log4jProperties;
+    }
+
+    public static void resetConfiguration() {
+        LogManager.resetConfiguration();
+    }
+
+}
