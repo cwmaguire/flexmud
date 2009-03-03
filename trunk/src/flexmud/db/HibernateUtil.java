6c614f9d16c8a0d320647bf075caa38aa68c0865
cwmaguire <cwmaguire@2b8fd586-012b-11de-85c2-bb0660c75f22>
2009-03-03 23:06:59 +0000

    Figured out how to use Copyright profiles in IDEA

--- trunk/src/flexmud/db/HibernateUtil.java
+++ trunk/src/flexmud/db/HibernateUtil.java
@@ -1,3 +1,20 @@
+/**************************************************************************************************
+ * Copyright 2009 Chris Maguire (cwmaguire@gmail.com)                                             *
+ *                                                                                                *
+ * Flexmud is free software: you can redistribute it and/or modify                                *
+ * it under the terms of the GNU General Public License as published by                           *
+ * the Free Software Foundation, either version 3 of the License, or                              *
+ * (at your option) any later version.                                                            *
+ *                                                                                                *
+ * Flexmud is distributed in the hope that it will be useful,                                     *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of                                 *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                  *
+ * GNU General Public License for more details.                                                   *
+ *                                                                                                *
+ * You should have received a copy of the GNU General Public License                              *
+ * along with flexmud.  If not, see <http://www.gnu.org/licenses/>.                               *
+ **************************************************************************************************/
+
 package flexmud.db;
 
 import org.hibernate.Criteria;
@@ -41,7 +58,7 @@ public class HibernateUtil {
 
         session = HibernateUtil.getSessionFactory().openSession();
         transaction = session.beginTransaction();
-        
+
         Criteria criteria = detachedCriteria.getExecutableCriteria(session);
 
         try{
