4ecd815e8055fd53b6b789db43bf2491ff027ff2
cwmaguire@gmail.com <cwmaguire@gmail.com@2b8fd586-012b-11de-85c2-bb0660c75f22>
2010-05-07 14:25:57 +0000

    Config Editor: set the edit controls on a couple cells in the Context Command table (the edits are currently thrown away)

--- src/flexmud/cfg/editor/gui/context/ContextCommandTablePanel.groovy
+++ src/flexmud/cfg/editor/gui/context/ContextCommandTablePanel.groovy
@@ -1,16 +1,16 @@
 package flexmud.cfg.editor.gui.context
 
 import flexmud.cfg.editor.Controller
-
 import flexmud.engine.context.Context
 import java.awt.Dimension
 import java.awt.GridLayout
+import javax.swing.DefaultCellEditor
 import javax.swing.JPanel
 import javax.swing.JScrollPane
-import javax.swing.JTree
-import javax.swing.tree.DefaultMutableTreeNode
-import javax.swing.tree.DefaultTreeModel
 import javax.swing.JTable
+import javax.swing.JTextField
+import javax.swing.JComboBox
+import flexmud.engine.context.ContextCommandFlag
 
 public class ContextCommandTablePanel extends JPanel {
   String title = "Commands"
@@ -35,6 +35,8 @@ public class ContextCommandTablePanel extends JPanel {
   public void load(Context context) {
     context.getContextCommands()
     table.setModel(new ContextCommandTableModel(context))
+    table.getColumnModel().getColumn(1).setCellEditor(new DefaultCellEditor(new JTextField()))
+    table.getColumnModel().getColumn(2).setCellEditor(new DefaultCellEditor(new JComboBox(ContextCommandFlag.values())))
     repaint()
   }
 
